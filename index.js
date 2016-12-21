9a654421fe3d45e11a23ca777f6cdf741b32aff6
Assis Ngolo <figassis@nellcorp.com>
2016-12-21 15:43:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var request = require('request');
+
+
+var options = {
+  url: 'https://raw.githubusercontent.com/figassis/servernames/master/names.json',
+  headers: { 'User-Agent': 'request' }
+};
+
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    request(options, function (error, response, body) {
+    	if (error || response.statusCode != 200) callback("Could not retrieve names file"); 
+  		var names = JSON.parse(body).servernames;
+  		var list = [];
+
+  		if(event.hasOwnProperty('topic') && names.hasOwnProperty(event.topic)){
+  			list = names[event.topic];
+  		}else{
+  			Object.getOwnPropertyNames(names).forEach(function(val, idx, array) {
+  				list = list.concat(names[val]);
+			});
+  		}
+    	
+    	var name = list[Math.floor(Math.random()*list.length)];
+    	var result = "Call your server: " + name + '.' + event.domain;
+    	//console.log( result );
+
+		callback(null, result);
+	});
+};
